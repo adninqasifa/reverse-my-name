ade88af68604e0ce23eaf90f06cb25b12ebe7d4e
Adnin Qasifa <adninqasifa@gmail.com>
2020-12-05 17:27:17 +0700

    Tugas Mock Tech Interview

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function reverseMyName(str) {
-    // put your code here
+// Reverse my Name
+// Exercise in Tech Interview Glints Academy
 
-    return str
+function reverseMyName(str) {
+  // put your code here
+  const reverseName = str.split('').reverse().join('');
+  return reverseName;
 }
 
 const Test = (fun, result) => console.log(reverseMyName(fun) === result)
@@ -10,3 +13,8 @@ Test("A", "A")
 Test("Michael Jackson","noskcaJ leahciM")
 Test("Alvian Zachry Faturrahman", "namharrutaF yrhcaZ naivlA")
 Test("", "")
+
+//console.log(reverseMyName("Michael Jackson"));
+//console.log(reverseMyName("Alvian Zachry Faturrahman"));
+
+// DONE
